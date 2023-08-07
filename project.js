ca3d9c9356064bf0be794af66171e4aa5a8c0849
Your Name <sandaruwanprabod@gmail.com>
2023-08-07 09:42:02 +0530

    first commit

---
+++ project.js
@@ -0,0 +1,11 @@
+// 1. Deposit some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Give the user their winings
+// 7. Play again
+
+function deposit() {
+    
+} 
