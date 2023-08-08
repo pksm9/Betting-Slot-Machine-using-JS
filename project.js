380285136bf3ab09ab6b64195d7da276aa158439
Your Name <sandaruwanprabod@gmail.com>
2023-08-08 17:17:18 +0530

    not working issue

--- project.js
+++ project.js
@@ -85,7 +85,76 @@ const spin = () => {
   return reels;
 };
 
-const reels = spin();
-const balance = deposit();
-const numberofLines = getNumberOfLines();
-const bet = getBet(balance, numberofLines);
+const transpose = (reels) => {
+  const rows = [];
+  for(let i = 0; i < ROWS; i++) {
+    rows.push([]);
+    for(j = 0; j < COLS; j++) {
+      rows[i].push(reels[j][i]);
+    }
+  }
+
+  return rows;
+};
+
+const printRows = (rows) => {
+  for(const row of rows) {
+    let rowString = "";
+    for(const [i, symbol] of rows.entries()) {
+      rowString += symbol;
+      if(i != row.length - 1){
+        rowString += " | ";
+      }
+    }
+    console.log(rowString);
+  }
+};
+
+const getWinnings = (rows, bet, lines) => {
+  let winnings = 0;
+
+  for(let row = 0; row < lines; row++) {
+    const symbols = rows[row];
+    let allSame = true;
+
+     for(const symbol of symbols) {
+      if(symbol != symbols[0]) {
+        allSame = false;
+        break;
+      }
+     }
+
+     if(allSame) {
+      winnings += bet * SYMBOL_VALUES[symbols[0]];
+     }
+  }
+
+  return winnings;
+}
+
+const game = () => {
+  let balance = deposit();
+
+  while(true) {
+    console.log("You have a balance of $" + balance);
+    const numberofLines = getNumberOfLines();
+    const bet = getBet(balance, numberofLines);
+    balance -= bet*numberofLines;
+    const reels = spin();
+    const rows = transpose(reels);
+    printRows(rows);
+    const winnings = getWinnings(rows, bet, numberofLines);
+    balance += winnings;
+    console.log("You won, $" + winnings.toString());
+
+    if(balance <= 0) {
+      console.log("You ran out of money!");
+      break;
+    }
+
+    const playAgain = prompt("Do you want to play (y/n)? ")
+    if(playAgain != "y") break;
+  }
+  
+}
+
